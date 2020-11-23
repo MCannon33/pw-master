289cf82f84e09c56fc25757eaf27f94e5577dac4
MCannon33 <marialcannon@gmail.com>
2020-11-22 22:00:24 -0500

    add password variables and update html to include js and css files

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,28 @@
 // Assignment code here
 
+//Use a window alert to create prompts for password criteria 
+
+  //Password Criteria Options 
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+  var characters = ["!","@","#","$","%","^","&","*","(",")"]
+  var alphaLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+//var passwordCriteria = window.prompt();{
+//var password = function(createpassword){
+  //var promptPwc = window.prompt("here is the password criteria; enter okay to confirm");
+  //if (promptPwc === "okay") || promptPwc === "OKAY"{
+    //var confirmOkay = window.prompt("Criteria 1")
+    //var confirmOkay = window.prompt("Criteria 2")
+    //var confirmOkay = window.prompt("Criteria 3")
+  //concole.log()
+    //}
+  //};
+  //else{
+    //window.alert("must have X# of lowercase letters")
+  //}
+
+  //var passwordCriteria = 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +38,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
